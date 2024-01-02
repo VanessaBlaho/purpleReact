109f41cdcb662134c9aa82c9f15c84bbf55e506a
Vanessa Blaho <vanessadblaho@gmail.com>
2024-01-02 11:45:26 +0100

    adding tab buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept';
 import { CORE_CONCEPTS } from './data';
+import TabButton from './components/TabButton';
 
 
 function App() {
+  function handleSelect (){
+    console.log('Hello World');
+}
   return (
     <div>
       <Header />
@@ -28,6 +32,15 @@ function App() {
            
             
           </ul>
+        </section>
+        <section id ="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
